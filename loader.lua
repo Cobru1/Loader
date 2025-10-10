c4ecabd6d0dec8c916e56608933229cc37c6e31b
Cobru1 <anshkmittal@gmail.com>
2025-10-11 03:09:50 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -20,7 +20,7 @@ local success, NotificationLib = pcall(function()
 end)
 
 if success and NotificationLib then
-    NotificationLib:Show("￢ﾚﾡ Loader", "Library loaded!", 3)
+    NotificationLib:Show("🔌 Loader", "Library loaded!", 3)
 
     local FIND_MARKERS_PLACEIDS = {
         10026123355,
@@ -32,6 +32,7 @@ if success and NotificationLib then
         8954347485
     }
     local RACING_SIM_PLACEID = 105483453326837
+    local ELEMENTAL_TYCOON_PLACEID = 10253248401
 
     local function findInTable(val, tab)
         for i=1,#tab do if tab[i]==val then return true end end
@@ -41,16 +42,19 @@ if success and NotificationLib then
     local placeId = game.PlaceId
 
     if findInTable(placeId, FIND_MARKERS_PLACEIDS) then
-        NotificationLib:Show("￰ﾟﾎﾯ Loader", "Game detected: Find the Markers", 5)
+        NotificationLib:Show("🎯 Loader", "Game detected: Find the Markers", 5)
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Cobru1/Find-the-markers/refs/heads/main/Find%20the%20markers.lua"))()
     elseif placeId == RACING_SIM_PLACEID then
-        NotificationLib:Show("￰ﾟﾏﾎ￯ﾸﾏ Loader", "Game detected: Racing Simulator", 5)
+        NotificationLib:Show("🏎️ Loader", "Game detected: Racing Simulator", 5)
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Cobru1/Racing-Simulator/refs/heads/main/racing%20simulator.lua"))()
+    elseif placeId == ELEMENTAL_TYCOON_PLACEID then
+        NotificationLib:Show("🌩️ Loader", "Game detected: Elemental Power Tycoon", 5)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Cobru1/Elemental-Power-Tycoon/refs/heads/main/Elemental%20Power%20Tycoon.lua"))()
     else
         local MarketplaceService = game:GetService("MarketplaceService")
         local gameName = "Unknown game"
         pcall(function() gameName = MarketplaceService:GetProductInfo(placeId).Name end)
-        NotificationLib:Show("￢ﾝﾌ Loader", "Game not supported: "..gameName, 6)
+        NotificationLib:Show("❌ Loader", "Game not supported: "..gameName, 6)
     end
 else
     warn("Failed to load Notification from GitHub!")
